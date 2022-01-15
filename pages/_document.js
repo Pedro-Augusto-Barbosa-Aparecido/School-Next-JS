33dfd244b9b449f799bc1a17c6fa5345f1a4df5e
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-14 21:22:24 -0300

    Fix bug in scripts botstrap third

--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,12 @@ import Document, {
 
 } from 'next/document';
 
-import Script from "next/script";
-
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
