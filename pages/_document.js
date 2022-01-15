cc6dd556519bb68b03efc122bd2fa1017e7d19b6
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-14 21:17:51 -0300

    Fix bug in scripts botstrap

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import Document, {
     Main,
     NextScript
 
-} from 'next/document'
+} from 'next/document';
+
+import Script from "next/script";
 
 export default class MyDocument extends Document {
     render() {
@@ -22,13 +24,13 @@ export default class MyDocument extends Document {
                           rel="stylesheet"
                           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
                           crossOrigin="anonymous" />
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
     crossOrigin="anonymous"/>
-                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+                    <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
     integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
     crossOrigin="anonymous"/>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+                    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
     integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
     crossOrigin="anonymous"/>
 
